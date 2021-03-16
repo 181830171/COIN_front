a01056e92f1d089d82ef9128fe6508ed429eff7b
181830171 <181830171@smail.nju.edu.cn>
2021-03-16 15:56:37 +0800

    highlight graph

--- src/main.js
+++ src/main.js
@@ -4,7 +4,4 @@ import vuex from 'vuex'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Vue from 'vue'
 createApp(App).use(store).use(router).use(vuex).mount('#app')
-// Vue.prototype.$axios=axios
-// axios.defaults.baseURL='/api'
