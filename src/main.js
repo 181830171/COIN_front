b1d02876b36d3b3ab4054bfa798cc4591fe31d2f
金宇航 <181250062@smail.nju.edu.cn>
2021-04-14 23:11:48 +0800

    add category

--- src/main.js
+++ src/main.js
@@ -5,12 +5,18 @@ import vuex from 'vuex'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Antd from 'ant-design-vue';
+import Antd from 'ant-design-vue'
+import {Form} from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
 import vcolorpicker from 'vcolorpicker'
 import qs from 'qs'
-createApp(App).use(store).use(router).use(Antd).use(vuex).use(qs).mount('#app')
 
+const app = createApp(App)
+app.use(store).use(router).use(Antd).use(vuex).use(qs)
+// app.prototype.$form = Form
+app.mount('#app')
+// createApp(App).use(store).use(router).use(Antd).use(vuex).use(qs).mount('#app')
+// Vue.use(Antd)
 // // The Vue build version to load with the `import` command
 // // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 // import Vue from 'vue'
